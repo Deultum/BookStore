5a906b1c4e1b6fcc8ddb205d91f884f1e8b017e6
Ivanov.I <ivanov.ivan.zhivkov@gmail.com>
2023-07-25 09:03:14 +0300

    Add user service

---
+++ bookstore/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bookstore/src/app/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor() { }
+
+  login(username: string, password: string) {
+    // Placeholder implementation for login
+    // You would typically send an API request to the server for authentication
+    return true;
+  }
+
+  register(username: string, email: string, password: string) {
+    // Placeholder implementation for user registration
+    // You would typically send an API request to the server to create a new user
+    return true;
+  }
+
+  getUserProfile() {
+    // Placeholder implementation to get user profile
+    // You would typically send an API request to the server to fetch user data
+    return {
+      username: 'JohnDoe',
+      email: 'john@example.com',
+      // Add more user-related information as needed
+    };
+  }
+}
